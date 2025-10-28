801268c5f91f57bbce701ad91db47ef719d591be
daeseopk <eotjqdlee@gmail.com>
2025-10-28 11:48:03 +0900

    chore: setup absolute import paths using module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
-  plugins: ["nativewind/babel"],
+  plugins: [
+    "nativewind/babel",
+    [
+      "module-resolver",
+      {
+        root: ["."],
+        alias: {
+          "@": "./src",
+        },
+      },
+    ],
+  ],
 };

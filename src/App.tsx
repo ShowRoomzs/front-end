8285538efae0d8b5a1cf59ccd279ceaaa2d23b8f
daeseopk <eotjqdlee@gmail.com>
2025-11-07 14:15:31 +0900

    feat: define App root structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { queryClient } from "./common/lib/queryClient";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white">Hello World</Text>
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView className="flex-1">
+        <SafeAreaProvider className="flex-1">
+          <InitApp />
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   );
+}
+
+function InitApp() {
+  return <View></View>;
 }

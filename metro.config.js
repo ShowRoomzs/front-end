51b68da5a5494b35cb77fda2c3815ac4e0099a1c
daeseopk <eotjqdlee@gmail.com>
2025-11-07 15:57:14 +0900

    feat: setup for foundation, svg

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,11 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg");
+config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"];
+
 config.resolver.extraNodeModules = {
   "@": path.resolve(__dirname, "src"),
 };

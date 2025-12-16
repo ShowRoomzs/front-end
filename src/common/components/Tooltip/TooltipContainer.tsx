438e25bb636bf2240b3d7d3b8bd8aad98b57435f
daeseopk <eotjqdlee@gmail.com>
2025-12-16 15:48:51 +0900

    feat: Introduce TooltipContainer component and update TooltipRenderer to utilize it

---
+++ src/common/components/Tooltip/TooltipContainer.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+import { TooltipInstance } from "@/common/providers/TooltipProvider";
+
+export default function TooltipContainer(props: TooltipInstance) {
+  const { config, isOpen, tooltipRef } = props;
+
+  return <View></View>;
+}

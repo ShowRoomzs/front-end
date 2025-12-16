4b2446f0830ffa9572d55d087e63781f7d3e2dcf
daeseopk <eotjqdlee@gmail.com>
2025-12-16 17:06:51 +0900

    chore: Remove unused TooltipContainer component

--- src/common/components/Tooltip/TooltipContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native";
-
-import { TooltipInstance } from "@/common/providers/TooltipProvider";
-
-export default function TooltipContainer(props: TooltipInstance) {
-  const { config, isOpen, tooltipRef } = props;
-
-  return <View></View>;
-}

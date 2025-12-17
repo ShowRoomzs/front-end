4b338ac937974bb1907539c3ca97a667868f6aaf
daeseopk <eotjqdlee@gmail.com>
2025-12-17 11:27:14 +0900

    refactor: Replace TooltipProvider with OverlayContextProvider to manage overlays

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { queryClient } from "./common/lib/queryClient";
-import { TooltipProvider } from "./common/providers/TooltipProvider";
+import OverlayContextProvider from "./common/providers/OverlayContextProvider";
 import MainNavigator from "./navigators/MainNavigator";
 
 export default function App() {
@@ -13,9 +13,9 @@ export default function App() {
       <GestureHandlerRootView className="flex-1">
         <SafeAreaProvider className="flex-1">
           <NavigationContainer>
-            <TooltipProvider>
+            <OverlayContextProvider>
               <MainNavigator />
-            </TooltipProvider>
+            </OverlayContextProvider>
           </NavigationContainer>
         </SafeAreaProvider>
       </GestureHandlerRootView>

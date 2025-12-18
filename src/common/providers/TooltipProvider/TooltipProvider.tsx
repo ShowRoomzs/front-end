f934289213c9a8ee79d7950a61d00253a3f7ed54
daeseopk <eotjqdlee@gmail.com>
2025-12-18 16:10:40 +0900

    refactor : split overlay, bottom sheet, tooltip providers

--- src/common/providers/TooltipProvider/TooltipProvider.tsx
+++ src/common/providers/TooltipProvider/TooltipProvider.tsx
@@ -1,26 +1,7 @@
-import { createContext, createRef, ReactNode, RefObject, useCallback, useContext, useState } from "react";
+import { createRef, ReactNode, RefObject, useCallback, useState } from "react";
 import { View } from "react-native";
 
-import { TooltipProps } from "../components/Tooltip/Tooltip";
-
-export type TooltipConfig = TooltipProps;
-
-export interface TooltipInstance {
-  config: TooltipConfig;
-  isOpen: boolean;
-  tooltipRef: RefObject<View | null>;
-}
-
-type TooltipContextValue = {
-  show: (tooltipId: string, config: TooltipConfig) => void;
-  hide: (tooltipId: string) => void;
-  hideAll: () => void;
-  getInstance: (tooltipId: string) => TooltipInstance | undefined;
-  getActiveInstances: () => Map<string, TooltipInstance>;
-  registerInstance: (tooltipId: string, ref: RefObject<View | null>) => void;
-};
-
-export const TooltipContext = createContext<TooltipContextValue | undefined>(undefined);
+import { TooltipContext, TooltipConfig, TooltipInstance } from "./context";
 
 interface TooltipProviderProps {
   children: ReactNode;
@@ -115,7 +96,7 @@ export function TooltipProvider(props: TooltipProviderProps) {
     });
   }, []);
 
-  const contextValue: TooltipContextValue = {
+  const contextValue = {
     show,
     hide,
     hideAll,
@@ -125,14 +106,4 @@ export function TooltipProvider(props: TooltipProviderProps) {
   };
 
   return <TooltipContext.Provider value={contextValue}>{children}</TooltipContext.Provider>;
-}
-
-export function useTooltipContext() {
-  const context = useContext(TooltipContext);
-
-  if (!context) {
-    throw new Error("useTooltipContext must be used within a TooltipProvider");
-  }
-
-  return context;
 }

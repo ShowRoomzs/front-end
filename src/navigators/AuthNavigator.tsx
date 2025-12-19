b73774de49bf4f66aecece5452ab401999e655d8
daeseopk <eotjqdlee@gmail.com>
2025-12-19 15:18:09 +0900

    refactor: remove MAIN_ROUTES and update navigators to use conditional rendering based on user authentication

--- src/navigators/AuthNavigator.tsx
+++ src/navigators/AuthNavigator.tsx
@@ -1,5 +1,16 @@
-import { View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { AUTH_ROUTES, AuthStackParamList } from "@/common/router";
+import AuthHomeView from "@/features/auth/views/AuthHomeView";
+import SignUpView from "@/features/auth/views/SignUpView";
+
+const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 export default function AuthNavigator() {
-  return <View className="flex-1"></View>;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={AUTH_ROUTES.HOME} component={AuthHomeView} />
+      <Stack.Screen name={AUTH_ROUTES.SIGN_UP} component={SignUpView} />
+    </Stack.Navigator>
+  );
 }

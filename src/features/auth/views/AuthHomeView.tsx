7e1f40cef66ad1facb35f680cedcc63c88194aac
daeseopk <eotjqdlee@gmail.com>
2025-12-23 17:59:00 +0900

    feat: implement swipe down gesture to close modal in AuthHomeView

--- src/features/auth/views/AuthHomeView.tsx
+++ src/features/auth/views/AuthHomeView.tsx
@@ -1,5 +1,7 @@
 import { useCallback, useMemo } from "react";
 import { Platform, Pressable, Text, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { runOnJS } from "react-native-reanimated";
 
 import Icon from "@/common/components/Icon/Icon";
 import VStack from "@/common/components/VStack/VStack";
@@ -25,22 +27,37 @@ export default function AuthHomeView() {
     // TODO : 서버 요청
   }, []);
 
+  const pan = useMemo(
+    () =>
+      Gesture.Pan().onEnd(e => {
+        const { velocityY, translationY } = e;
+        // 스와이프 다운 시 모달 닫기
+
+        if (velocityY > 100 && translationY > 10) {
+          runOnJS(navigation.goBack)();
+        }
+      }),
+    [navigation.goBack]
+  );
+
   return (
-    <View className="bg-black flex-1 flex items-center justify-center">
-      <VStack gap={45} className="w-full">
-        <Icon icon={COMMON_ASSETS.logo} className="self-center" />
-        <VStack gap={20} className="w-full px-20">
-          {socialButtons.map(socialType => (
-            <SocialButton onPress={handlePressSocialButton} key={socialType} socialType={socialType} />
-          ))}
+    <GestureDetector gesture={pan}>
+      <View className="bg-black flex-1 flex items-center justify-center">
+        <VStack gap={45} className="w-full">
+          <Icon icon={COMMON_ASSETS.logo} className="self-center" />
+          <VStack gap={20} className="w-full px-20">
+            {socialButtons.map(socialType => (
+              <SocialButton onPress={handlePressSocialButton} key={socialType} socialType={socialType} />
+            ))}
+          </VStack>
+          <Pressable
+            className="bg-white w-100 h-50"
+            onPress={() => navigation.navigate(AUTH_ROUTES.SIGN_UP, {})}
+          >
+            <Text>회원가입 라우팅</Text>
+          </Pressable>
         </VStack>
-        <Pressable
-          className="bg-white w-100 h-50"
-          onPress={() => navigation.navigate(AUTH_ROUTES.SIGN_UP, {})}
-        >
-          <Text>회원가입 라우팅</Text>
-        </Pressable>
-      </VStack>
-    </View>
+      </View>
+    </GestureDetector>
   );
 }

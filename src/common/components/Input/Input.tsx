91d6ddf7780fec0e1304fdef0b38a36dff09e21b
daeseopk <eotjqdlee@gmail.com>
2025-12-23 17:39:57 +0900

    feat: add forwardRef to Input and LabeledInput

--- src/common/components/Input/Input.tsx
+++ src/common/components/Input/Input.tsx
@@ -1,12 +1,13 @@
-import { ReactNode } from "react";
+import { forwardRef, ReactNode, useEffect, useState } from "react";
 import { TextInput, TextInputProps, View } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 
 import Typography from "@/common/components/Typography/Typography";
-import VStack from "@/common/components/VStack/VStack";
 import { cn } from "@/common/utils/cn";
 
 type InputSize = "small" | "medium";
 type InputStatus = "default" | "error" | "success";
+
 export interface InputProps extends Omit<TextInputProps, "className"> {
   wrapperClassName?: string;
   inputClassName?: string;
@@ -16,7 +17,7 @@ export interface InputProps extends Omit<TextInputProps, "className"> {
   status?: InputStatus;
 }
 
-export default function Input(props: InputProps) {
+const Input = forwardRef<TextInput, InputProps>((props, ref) => {
   const {
     size = "medium",
     renderPreFix,
@@ -26,6 +27,48 @@ export default function Input(props: InputProps) {
     inputClassName,
     ...inputProps
   } = props;
+
+  const [displayHelperText, setDisplayHelperText] = useState(helperText);
+  const [textHeight, setTextHeight] = useState(0);
+
+  const opacity = useSharedValue(helperText ? 1 : 0);
+  const marginTop = useSharedValue(helperText ? 10 : 0);
+  const height = useSharedValue(helperText ? -1 : 0);
+
+  useEffect(() => {
+    if (helperText) {
+      // 텍스트가 있으면 바로 보여주기 시작
+      setDisplayHelperText(helperText);
+      opacity.value = withTiming(1, { duration: 200 });
+      marginTop.value = withTiming(10, { duration: 200 });
+      height.value = withTiming(textHeight || -1, { duration: 200 });
+    } else if (displayHelperText) {
+      // 텍스트가 없어지면 페이드아웃
+      opacity.value = withTiming(0, { duration: 150 });
+      marginTop.value = withTiming(0, { duration: 150 });
+      height.value = withTiming(0, { duration: 150 });
+    }
+  }, [helperText, opacity, marginTop, height, textHeight, displayHelperText]);
+
+  const handleLayout = (event: { nativeEvent: { layout: { height: number } } }) => {
+    const layoutHeight = event.nativeEvent.layout.height;
+
+    if (layoutHeight > 0 && textHeight === 0) {
+      setTextHeight(layoutHeight);
+      if (helperText) {
+        height.value = layoutHeight;
+      }
+    }
+  };
+
+  const animatedHelperStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+      marginTop: marginTop.value,
+      height: height.value === -1 ? undefined : height.value,
+      overflow: "hidden",
+    };
+  });
 
   const getDefaultClassName = () => {
     return "flex flex-row items-center w-full";
@@ -41,25 +84,38 @@ export default function Input(props: InputProps) {
   };
 
   const getHelperTextClassName = () => {
+    let className = "text-[12px] font-[400]";
     switch (status) {
       case "error":
-        // TODO
-        return "";
+        className += " text-negativeColor";
+        break;
       case "success":
         // TODO
-        return "";
+        break;
       case "default":
-        return "text-[12px] text-gray8 font-[400]";
+        className += " text-gray8";
+        break;
     }
+    return className;
   };
 
   return (
-    <VStack gap={helperText ? 10 : 0}>
+    <View>
       <View className={cn(getDefaultClassName(), getClassNameBySize(), wrapperClassName)}>
         {renderPreFix && <View className="mr-10">{renderPreFix}</View>}
-        <TextInput className={cn("flex-1", inputClassName)} {...inputProps} />
+        <TextInput ref={ref} className={cn("flex-1", inputClassName)} {...inputProps} />
       </View>
-      {helperText && <Typography className={cn(getHelperTextClassName())}>{helperText}</Typography>}
-    </VStack>
+      {(helperText || displayHelperText) && (
+        <Animated.View style={animatedHelperStyle}>
+          <View onLayout={handleLayout}>
+            <Typography className={cn(getHelperTextClassName())}>{displayHelperText}</Typography>
+          </View>
+        </Animated.View>
+      )}
+    </View>
   );
-}
+});
+
+Input.displayName = "Input";
+
+export default Input;

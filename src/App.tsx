1899feb77a5eecacbd8a962fa5d04a49b482ccb5
daeseopk <eotjqdlee@gmail.com>
2025-12-23 15:54:08 +0900

    refactor: restructure SafeAreaView provider hierarchy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { queryClient } from "@/common/lib/queryClient";
 import OverlayProvider from "@/common/providers/OverlayProvider";
@@ -19,15 +19,13 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView className="flex-1">
-        <SafeAreaProvider className="flex-1">
-          <NavigationContainer theme={THEME}>
-            <OverlayProvider>
-              <SafeAreaView className="flex-1">
-                <MainNavigator />
-              </SafeAreaView>
-            </OverlayProvider>
-          </NavigationContainer>
-        </SafeAreaProvider>
+        <OverlayProvider>
+          <SafeAreaProvider className="flex-1">
+            <NavigationContainer theme={THEME}>
+              <MainNavigator />
+            </NavigationContainer>
+          </SafeAreaProvider>
+        </OverlayProvider>
       </GestureHandlerRootView>
     </QueryClientProvider>
   );

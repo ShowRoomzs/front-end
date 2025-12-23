51f2652bf347800a68f57368066ee05d89916d64
daeseopk <eotjqdlee@gmail.com>
2025-12-23 15:53:50 +0900

    refactor: update Terms modal presentation and header

--- src/features/auth/components/TermsHeader/TermsHeader.tsx
+++ src/features/auth/components/TermsHeader/TermsHeader.tsx
@@ -1,6 +1,5 @@
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import { TouchableOpacity } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import Header from "@/common/components/Header/Header";
 import Icon from "@/common/components/Icon/Icon";
@@ -10,17 +9,15 @@ import { COMMON_ASSETS } from "@/common/utils/assets";
 
 export default function TermsHeader(props: NativeStackHeaderProps) {
   const { navigation, route } = props;
-  const inset = useSafeAreaInsets();
   const termsType = (route.params as AuthStackParamList[typeof AUTH_ROUTES.TERMS]).termsType;
   const title = termsType === "privacy" ? "개인정보처리방침" : "서비스 이용 약관";
 
   return (
     <Header
-      style={{ paddingTop: inset.top }}
-      className="px-20"
-      renderLeft={
+      className="px-20 py-20"
+      renderRight={
         <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.goBack()}>
-          <Icon icon={COMMON_ASSETS.back} width={24} height={24} />
+          <Icon icon={COMMON_ASSETS.closeBlack} />
         </TouchableOpacity>
       }
       title={title}

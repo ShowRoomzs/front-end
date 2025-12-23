7435fc6c1cf1b58316d7869924c910e149db0da1
daeseopk <eotjqdlee@gmail.com>
2025-12-23 16:29:47 +0900

    refactor: adjust HomeTabs header height

--- src/features/auth/components/SignUpHeader/SignUpHeader.tsx
+++ src/features/auth/components/SignUpHeader/SignUpHeader.tsx
@@ -1,3 +1,4 @@
+import { CommonActions } from "@react-navigation/native";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import { TouchableOpacity } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -12,12 +13,21 @@ export default function SignUpHeader(props: SignUpHeaderProps) {
   const { navigation } = props;
   const inset = useSafeAreaInsets();
 
+  const handlePressBack = () => {
+    // Auth 모달 닫기
+    const parent = navigation.getParent();
+
+    if (parent) {
+      parent.dispatch(CommonActions.goBack());
+    }
+  };
+
   return (
     <Header
       style={{ paddingTop: inset.top }}
       className="px-20"
       renderLeft={
-        <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.goBack()}>
+        <TouchableOpacity activeOpacity={0.5} onPress={handlePressBack}>
           <Icon icon={COMMON_ASSETS.back} width={24} height={24} />
         </TouchableOpacity>
       }

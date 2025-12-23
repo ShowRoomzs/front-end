ce04bf31b915fc67f5715e11edca916338cf1571
daeseopk <eotjqdlee@gmail.com>
2025-12-23 15:53:59 +0900

    refactor: remove unused right section from SignUpHeader

--- src/features/auth/components/SignUpHeader/SignUpHeader.tsx
+++ src/features/auth/components/SignUpHeader/SignUpHeader.tsx
@@ -4,7 +4,6 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import Header from "@/common/components/Header/Header";
 import Icon from "@/common/components/Icon/Icon";
-import Typography from "@/common/components/Typography/Typography";
 import { COMMON_ASSETS } from "@/common/utils/assets";
 
 type SignUpHeaderProps = NativeStackHeaderProps;
@@ -22,7 +21,6 @@ export default function SignUpHeader(props: SignUpHeaderProps) {
           <Icon icon={COMMON_ASSETS.back} width={24} height={24} />
         </TouchableOpacity>
       }
-      renderRight={<Typography className="text-14 text-black">계정이 있으신가요?</Typography>}
     />
   );
 }

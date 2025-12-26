0c2f545710f15471ae7f7c41d5dfdf00a85a7c55
daeseopk <eotjqdlee@gmail.com>
2025-12-26 14:26:45 +0900

    feat: add dayjs

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import { registerRootComponent } from "expo";
 
 import App from "./src/App";
@@ -5,4 +7,5 @@ import App from "./src/App";
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+dayjs.extend(customParseFormat);
 registerRootComponent(App);

3d98741fd261f4f329851dad47597f3d69bd7151
daeseopk <eotjqdlee@gmail.com>
2025-12-26 16:20:01 +0900

    refactor: enhance terms view

--- src/features/auth/components/TermsHeader/TermsHeader.tsx
+++ src/features/auth/components/TermsHeader/TermsHeader.tsx
@@ -1,4 +1,5 @@
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
+import { useMemo } from "react";
 import { TouchableOpacity } from "react-native";
 
 import Header from "@/common/components/Header/Header";
@@ -10,7 +11,17 @@ import { COMMON_ASSETS } from "@/common/utils/assets";
 export default function TermsHeader(props: NativeStackHeaderProps) {
   const { navigation, route } = props;
   const termsType = (route.params as AuthStackParamList[typeof AUTH_ROUTES.TERMS]).termsType;
-  const title = termsType === "privacy" ? "개인정보처리방침" : "서비스 이용 약관";
+
+  const title = useMemo(() => {
+    switch (termsType) {
+      case "marketing":
+        return "마케팅 목적의 개인정보 수집, 이용 동의";
+      case "service":
+        return "서비스 이용 약관";
+      case "privacy":
+        return "개인정보처리방침";
+    }
+  }, [termsType]);
 
   return (
     <Header
--- src/features/auth/views/TermsView.tsx
+++ src/features/auth/views/TermsView.tsx
@@ -1,4 +1,5 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useMemo } from "react";
 import { View } from "react-native";
 
 import Typography from "@/common/components/Typography/Typography";
@@ -12,10 +13,20 @@ export default function TermsView(
 ) {
   const { route } = props;
   const { termsType } = route.params;
+  const content = useMemo(() => {
+    switch (termsType) {
+      case "marketing":
+        return <Typography>마케팅 목적의 개인정보 수집, 이용 동의</Typography>;
+      case "service":
+        return <Typography>서비스 이용 약관</Typography>;
+      case "privacy":
+        return <Typography>개인정보처리방침</Typography>;
+    }
+  }, [termsType]);
 
   return (
     <View>
-      <Typography>{termsType === "privacy" ? "개인정보처리방침" : "서비스 이용 약관"}</Typography>
+      <Typography>{content}</Typography>
     </View>
   );
 }

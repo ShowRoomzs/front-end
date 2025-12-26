12577018f07a73734bff9eb232f4ae7fcdb6664d
daeseopk <eotjqdlee@gmail.com>
2025-12-26 16:19:20 +0900

    refactor: improve checkbox components

--- src/features/auth/components/AuthTermsCheckboxGroup/AuthTermsCheckboxGroup.tsx
+++ src/features/auth/components/AuthTermsCheckboxGroup/AuthTermsCheckboxGroup.tsx
@@ -4,12 +4,11 @@ import TermsCheckboxGroup, { TermsItem } from "@/common/components/TermsCheckbox
 import { AUTH_ROUTES, useAuthNavigation } from "@/common/router";
 
 interface AuthTermsCheckboxGroupProps {
-  allCheckLabel?: string;
   onChange: (isAllChecked: boolean) => void;
 }
 
 export default function AuthTermsCheckboxGroup(props: AuthTermsCheckboxGroupProps) {
-  const { allCheckLabel = "전체 동의", onChange } = props;
+  const { onChange } = props;
   const navigation = useAuthNavigation();
 
   const termsItems: Array<TermsItem> = useMemo(
@@ -28,7 +27,7 @@ export default function AuthTermsCheckboxGroup(props: AuthTermsCheckboxGroupProp
       },
       {
         id: "marketing",
-        label: "마케팅 활용 동의",
+        label: "마케팅 목적의 개인정보 수집, 이용 동의",
         required: false,
         onPressView: () => navigation.navigate(AUTH_ROUTES.TERMS, { termsType: "marketing" }),
       },
@@ -36,5 +35,5 @@ export default function AuthTermsCheckboxGroup(props: AuthTermsCheckboxGroupProp
     [navigation]
   );
 
-  return <TermsCheckboxGroup items={termsItems} allCheckLabel={allCheckLabel} onChange={onChange} />;
+  return <TermsCheckboxGroup items={termsItems} onChange={onChange} />;
 }

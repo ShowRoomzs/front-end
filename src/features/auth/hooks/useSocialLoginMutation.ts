2fea8979d2f0a92a4c4002fcd12b48751eb1b53d
daeseopk <eotjqdlee@gmail.com>
2025-12-29 10:16:10 +0900

    feat: add auth hooks

--- src/features/auth/hooks/useSocialLoginMutation.ts
+++ src/features/auth/hooks/useSocialLoginMutation.ts
@@ -1,15 +1,9 @@
 import { useMutation } from "@tanstack/react-query";
 
-import type { ErrorResponse } from "@/common/types/error";
-
-import {
-  authService,
-  type SocialLoginRequest,
-  type SocialLoginResponse,
-} from "@/features/auth/services/authService";
+import { authService } from "@/features/auth/services/authService";
 
 export function useSocialLoginMutation() {
-  const socialLoginMutation = useMutation<SocialLoginResponse, ErrorResponse, SocialLoginRequest>({
+  const socialLoginMutation = useMutation({
     mutationFn: authService.socialLogin,
   });
 

fd919ddd4e81e947314d7fd5ee5cb4a449d4e7d1
daeseopk <eotjqdlee@gmail.com>
2025-12-29 15:56:48 +0900

    feat: add cart, notification, search, setting routes

---
+++ src/features/notification/views/NotificationView.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import Typography from "@/common/components/Typography/Typography";
+
+export default function NotificationView() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Typography>알림</Typography>
+    </View>
+  );
+}
---
+++ src/features/search/views/SearchView.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import Typography from "@/common/components/Typography/Typography";
+
+export default function SearchView() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Typography>검색</Typography>
+    </View>
+  );
+}

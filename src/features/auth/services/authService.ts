43f3d0c0df35107e05007f401b86c2795e1260b4
daeseopk <eotjqdlee@gmail.com>
2026-01-21 13:55:32 +0900

    refactor: reorganize type imports and clean up auth and user service files

--- src/features/auth/services/authService.ts
+++ src/features/auth/services/authService.ts
@@ -1,34 +1,7 @@
 import { authInstance } from "@/common/lib/authInstance";
 import { refreshInstance } from "@/common/lib/refreshInstance";
-import { Gender } from "@/common/types/gender";
-
-export interface SocialLoginRequest {
-  providerType: string;
-  token: string;
-  name?: string;
-  fcmToken?: string;
-}
-export interface SocialLoginResponse extends RegisterResponse {
-  isNewMember: boolean;
-  registerToken?: string;
-}
-
-export interface RegisterRequest {
-  nickname: string;
-  gender: Gender;
-  birthday: string;
-  serviceAgree: boolean;
-  privacyAgree: boolean;
-  marketingAgree: boolean;
-}
-
-export interface RegisterResponse {
-  tokenType: string;
-  accessToken: string;
-  refreshToken: string;
-  accessTokenExpiresIn: number;
-  refreshTokenExpiresIn: number;
-}
+import { SocialLoginResponse } from "@/features/auth/hooks/useSocialLogin";
+import { RegisterRequest, RegisterResponse, SocialLoginRequest } from "@/features/auth/types/auth";
 
 export const authService = {
   socialLogin: async (request: SocialLoginRequest): Promise<SocialLoginResponse> => {

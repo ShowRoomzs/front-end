50c3c28f299bc93fb4ed06cc06d0bfd4d50943b1
daeseopk <eotjqdlee@gmail.com>
2026-01-21 14:04:11 +0900

    feat: add category service

---
+++ src/features/auth/types/category.ts
@@ -0,0 +1,31 @@
+type FilterCondition = "OR" | "AND";
+export type FilterType = "CHECKBOX" | "COLOR" | "RANGE" | "BRAND"; // TODO : 타입 추가
+
+export interface FilterValue {
+  id: number;
+  value: string;
+  label: string;
+  extra: string | null;
+  sortOrder: number;
+  isActive: boolean;
+}
+
+export interface Filter {
+  id: number;
+  filterKey: string;
+  label: string;
+  filterType: FilterType;
+  condition: FilterCondition;
+  sortOrder: number;
+  isActive: boolean;
+  values: Array<FilterValue>;
+}
+
+export interface Category {
+  categoryId: number;
+  name: string;
+  order: number;
+  iconUrl: string;
+  parentId: string | null;
+  filters: Array<Filter>;
+}

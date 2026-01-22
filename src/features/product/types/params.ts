40be027dd79fc8e19c3dec4a928945489501762b
daeseopk <eotjqdlee@gmail.com>
2026-01-22 18:07:38 +0900

    feat: implement product listing

---
+++ src/features/product/types/params.ts
@@ -0,0 +1,57 @@
+import { Gender } from "@/common/types/gender";
+import { PageParams, PageResponse } from "@/common/types/page";
+
+export interface FilterParam {
+  key: string;
+  values: Array<string>;
+}
+
+export interface ProductListParams extends PageParams {
+  q: string; // 검색 조건
+  categoryId: number | null; // 카테고리 아이디
+  marketId: number | null; // 쇼룸 아이디
+  filters: Array<FilterParam>;
+}
+
+interface ProductPrice {
+  regularPrice: number;
+  discountRate: number;
+  salePrice: number;
+  maxBenefitPrice: number;
+}
+
+interface ProductStatus {
+  isOutOfStock: boolean;
+  isOutOfStockForced: boolean;
+}
+export interface Product {
+  categoryId: number;
+  categoryName: string;
+  createdAt: string;
+  deliveryEstimatedDays: number;
+  deliveryFee: number;
+  deliveryFreeThreshold: number;
+  deliveryType: string; // TODO : 타입 정의
+  description: string;
+  gender: Gender; // ? 왜있지
+  id: number;
+  isDisplay: boolean;
+  isRecommended: boolean;
+  isWished: boolean;
+  likeCount: number;
+  marketId: number;
+  marketName: "number";
+  name: number;
+  price: ProductPrice;
+  productNotice: string; // JSON 형태 > 파싱 필요
+  productNumber: string;
+  purchasePrice: number;
+  representativeImageUrl: string;
+  reviewCount: number;
+  sellerProductCode: string;
+  status: ProductStatus;
+  tags: string | null; // JSON 형태 > 파싱 필요
+  thumbnailUrl: string;
+}
+
+export type ProductListResponse = PageResponse<Product>;

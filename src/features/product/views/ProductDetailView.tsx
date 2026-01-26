b2acc9261639084fa617a788c010a4f921e4f4de
daeseopk <eotjqdlee@gmail.com>
2026-01-26 10:12:32 +0900

    feat: add ProductDetailHeader component and integrate it into ProductDetailView

--- src/features/product/views/ProductDetailView.tsx
+++ src/features/product/views/ProductDetailView.tsx
@@ -1,15 +1,40 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
-import { Text, View } from "react-native";
+import { useCallback } from "react";
+import { View } from "react-native";
 
-import { COMMON_ROUTES } from "@/common/router/routes";
+import { useMainNavigation } from "@/common/router";
+import { COMMON_ROUTES, ROOT_ROUTES } from "@/common/router/routes";
 import { CommonStackParamList } from "@/common/router/types";
+import ProductDetailHeader from "@/features/product/components/ProductDetailHeader/ProductDetailHeader";
 
 export default function ProductDetailView() {
-  const route = useRoute<RouteProp<CommonStackParamList, typeof COMMON_ROUTES.PRODUCT_DETAIL>>();
+  const { params } = useRoute<RouteProp<CommonStackParamList, typeof COMMON_ROUTES.PRODUCT_DETAIL>>();
+  const { productId } = params;
+
+  const navigation = useMainNavigation();
+  const handlePressBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  const handlePressSearch = useCallback(() => {
+    navigation.navigate(ROOT_ROUTES.COMMON, {
+      screen: COMMON_ROUTES.SEARCH,
+    });
+  }, [navigation]);
+
+  const handlePressCart = useCallback(() => {
+    navigation.navigate(ROOT_ROUTES.COMMON, {
+      screen: COMMON_ROUTES.CART,
+    });
+  }, [navigation]);
 
   return (
-    <View>
-      <Text>asdf</Text>
+    <View className="flex-1">
+      <ProductDetailHeader
+        onPressBack={handlePressBack}
+        onPressSearch={handlePressSearch}
+        onPressCart={handlePressCart}
+      />
     </View>
   );
 }

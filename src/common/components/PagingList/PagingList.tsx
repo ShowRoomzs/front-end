4352e47bd625b71c672db99809b12937beed8093
daeseopk <eotjqdlee@gmail.com>
2026-01-28 17:35:34 +0900

    refactor: PagingList onPageChange to onLoadMore

--- src/common/components/PagingList/PagingList.tsx
+++ src/common/components/PagingList/PagingList.tsx
@@ -9,23 +9,15 @@ import { PageInfo } from "@/common/types/page";
 interface PagingListProps<T> extends Omit<FlatListProps<T>, "data"> {
   data: Array<T> | undefined;
   pageInfo: PageInfo | undefined;
-  onPageChange: (page: number) => void;
+  onLoadMore: () => void;
   isLoading: boolean;
 }
-
-/**
- * @description 페이징 처리를 위한 리스트 컴포넌트
- * @param query 쿼리 함수
- * @param params 파라미터 (from useParams hook)
- * @param updateParams 파라미터 업데이트 함수 (from useParams hook)
- * @param flatListProps FlatList 컴포넌트의 속성 (ex. renderItem, etc.)
- */
 
 export default function PagingList<T>(props: PagingListProps<T>) {
   const {
     data,
     pageInfo,
-    onPageChange,
+    onLoadMore,
     isLoading,
     onEndReachedThreshold = DEFAULT_ON_END_REACHED_THRESHOLD,
     ...flatListProps
@@ -39,15 +31,15 @@ export default function PagingList<T>(props: PagingListProps<T>) {
         return;
       }
 
-      const { hasNext, isLast, currentPage } = pageInfo;
+      const { hasNext, isLast } = pageInfo;
 
       if (!hasNext || isLast) {
         return;
       }
 
-      onPageChange(currentPage + 1);
+      onLoadMore();
     },
-    [isLoading, onPageChange, pageInfo, props]
+    [isLoading, onLoadMore, pageInfo, props]
   );
 
   return (

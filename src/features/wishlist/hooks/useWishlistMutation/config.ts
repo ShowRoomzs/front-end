3490b3ed6a0a3311b666bce12ae609f14b3834a6
daeseopk <eotjqdlee@gmail.com>
2026-01-29 17:25:14 +0900

    refactor: split wishlist into useAddWishlistMutation, useDeleteWishlistMutation, move useUpdateWishlist to hooks

---
+++ src/features/wishlist/hooks/useWishlistMutation/config.ts
@@ -0,0 +1,10 @@
+import { queryClient } from "@/common/lib/queryClient";
+import { PRODUCT_QUERY_KEY } from "@/features/product/constants/queryKey";
+
+export function getInvalidateFns(productId: number) {
+  return [
+    () => queryClient.invalidateQueries({ queryKey: [PRODUCT_QUERY_KEY.PRODUCT_DETAIL, productId] }),
+    () => queryClient.invalidateQueries({ queryKey: [PRODUCT_QUERY_KEY.PRODUCTS] }),
+    () => queryClient.invalidateQueries({ queryKey: [PRODUCT_QUERY_KEY.RELATED_PRODUCTS, productId] }),
+  ];
+}

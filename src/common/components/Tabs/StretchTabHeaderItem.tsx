83e77a96d94950bca17aeca9329b66e8235720a7
daeseopk <eotjqdlee@gmail.com>
2026-01-29 11:19:24 +0900

    feat: enhance StretchTabHeaderItem to support customizable classNames

--- src/common/components/Tabs/StretchTabHeaderItem.tsx
+++ src/common/components/Tabs/StretchTabHeaderItem.tsx
@@ -8,15 +8,24 @@ interface StretchTabHeaderItemProps {
   item: TabItemType;
   itemCount: number;
   isActive: boolean;
+  wrapperClassName?: string;
+  labelClassName?: string;
+  activeLabelClassName?: string;
 }
 
 export default function StretchTabHeaderItem(props: StretchTabHeaderItemProps) {
-  const { item, itemCount, isActive } = props;
+  const { item, itemCount, isActive, wrapperClassName, labelClassName, activeLabelClassName } = props;
   const width = useWindowDimensions().width / itemCount;
 
   return (
-    <View className="py-15 flex items-center justify-center" style={{ width }}>
-      <Typography className={cn("text-13 font-normal", isActive && "text-black font-medium")}>
+    <View className={cn("py-15 flex items-center justify-center", wrapperClassName)} style={{ width }}>
+      <Typography
+        className={cn(
+          "text-13 font-normal",
+          labelClassName,
+          isActive && (activeLabelClassName ? activeLabelClassName : "text-black font-medium")
+        )}
+      >
         {item.label}
       </Typography>
     </View>

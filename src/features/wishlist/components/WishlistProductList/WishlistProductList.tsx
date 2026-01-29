01d4c021d3c3557579e6130ae2eada17d5042558
daeseopk <eotjqdlee@gmail.com>
2026-01-29 18:24:29 +0900

    refactor: wishlist product list and types

--- src/features/wishlist/components/WishlistProductList/WishlistProductList.tsx
+++ src/features/wishlist/components/WishlistProductList/WishlistProductList.tsx
@@ -1,22 +1,30 @@
 import { useCallback, useEffect, useRef } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 import type { WishlistProductType } from "@/features/wishlist/types/wishlist";
 
 import PagingList from "@/common/components/PagingList/PagingList";
 import { useParams } from "@/common/hooks/useParams";
+import { useMainNavigation } from "@/common/router";
+import { COMMON_ROUTES, ROOT_ROUTES } from "@/common/router/routes";
+import { CleanupFn } from "@/common/types/cleanup";
+import ProductCard from "@/features/product/components/ProductCard/ProductCard";
 import {
   WISHLIST_PRODUCT_LIST_GAP,
+  WISHLIST_PRODUCT_LIST_ITEM_WIDTH,
   WISHLIST_PRODUCT_LIST_NUM_OF_COLUMNS,
   WISHLIST_PRODUCT_LIST_PADDING_HORIZONTAL,
   WISHLIST_PRODUCT_LIST_PADDING_VERTICAL,
 } from "@/features/wishlist/components/WishlistProductList/config";
 import { useGetWishlist } from "@/features/wishlist/hooks/useGetWishlist";
+import { useUpdateWishlist } from "@/features/wishlist/hooks/useUpdateWishlist";
 import { WishlistParams } from "@/features/wishlist/types/params";
 
 interface WishlistProductListProps {
   categoryId: number | null;
   onLoad?: (products: Array<WishlistProductType>) => void;
+  // 좋아요 업데이트 시 외부로 cleanup 함수 전달(view단에서 뒷정리 처리하기 위함)
+  onUpdateCallback?: (cleanupFns: Array<CleanupFn>) => void;
 }
 const INITIAL_PARAMS: WishlistParams = {
   page: 1,
@@ -25,7 +33,9 @@ const INITIAL_PARAMS: WishlistParams = {
 };
 
 export default function WishlistProductList(props: WishlistProductListProps) {
-  const { categoryId, onLoad } = props;
+  const { categoryId, onLoad, onUpdateCallback } = props;
+  const { update: updateWishlist, cleanupFns } = useUpdateWishlist();
+  const navigation = useMainNavigation();
   const isMounted = useRef(false);
   const { params } = useParams<WishlistParams>({
     ...INITIAL_PARAMS,
@@ -42,18 +52,46 @@ export default function WishlistProductList(props: WishlistProductListProps) {
     isMounted.current = true;
   }, [products, onLoad, isLoading]);
 
-  //   const handlePressProduct = useCallback((product: WishlistProduct) => {
-  //     console.log(product);
-  //   }, []);
+  useEffect(() => {
+    if (!cleanupFns?.length || !onUpdateCallback) {
+      return;
+    }
+    onUpdateCallback(cleanupFns);
+  }, [cleanupFns, onUpdateCallback]);
 
-  //   const handlePressLike = useCallback((productId: number, newIsWished: boolean) => {
-  //     console.log(productId, newIsWished);
-  //   }, []);
+  const handlePressProduct = useCallback(
+    (product: WishlistProductType) => {
+      navigation.navigate(ROOT_ROUTES.COMMON, {
+        screen: COMMON_ROUTES.PRODUCT_DETAIL,
+        params: {
+          productId: product.id,
+        },
+      });
+    },
+    [navigation]
+  );
 
-  const renderItem = useCallback(({ item }: { item: WishlistProductType }) => {
-    void item;
-    return <Text>wishlist product dto 변경후 작업</Text>;
-  }, []);
+  const handlePressLike = useCallback(
+    (productId: number, newIsWished: boolean) => {
+      updateWishlist(productId, newIsWished);
+    },
+    [updateWishlist]
+  );
+
+  const renderItem = useCallback(
+    ({ item }: { item: WishlistProductType }) => {
+      return (
+        <ProductCard
+          product={item}
+          onPressLike={handlePressLike}
+          onPress={handlePressProduct}
+          width={WISHLIST_PRODUCT_LIST_ITEM_WIDTH}
+          useOptimisticUpdate={true}
+        />
+      );
+    },
+    [handlePressLike, handlePressProduct]
+  );
 
   const handleLoadMore = useCallback(() => {
     fetchNextPage();

8412e0261dff794d2b8efecc866a9bd94f1f8a9e
daeseopk <eotjqdlee@gmail.com>
2026-01-29 13:56:41 +0900

    feat: integrate user store management in refreshInstance for token handling

--- src/common/lib/refreshInstance.ts
+++ src/common/lib/refreshInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 import { SECURE_STORE } from "@/common/constants/secureStore";
+import { useUserStore } from "@/common/stores/useUserStore";
 
 export const refreshInstance = axios.create({
   baseURL: `${process.env.EXPO_PUBLIC_PROTOCOL}://${process.env.EXPO_PUBLIC_API_HOST}/v1/auth`,
@@ -9,11 +10,14 @@ export const refreshInstance = axios.create({
 
 refreshInstance.interceptors.response.use(
   res => res,
-  error => {
+  async error => {
     // TODO : 리프레시 토큰 만료 시 처리
     if (error.response.data.status === 401) {
-      SecureStore.deleteItemAsync(SECURE_STORE.REFRESH_TOKEN);
-      SecureStore.deleteItemAsync(SECURE_STORE.ACCESS_TOKEN);
+      const { clear } = useUserStore.getState();
+
+      await SecureStore.deleteItemAsync(SECURE_STORE.REFRESH_TOKEN);
+      await SecureStore.deleteItemAsync(SECURE_STORE.ACCESS_TOKEN);
+      clear();
     }
     return Promise.reject(error);
   }

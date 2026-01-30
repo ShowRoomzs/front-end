209d1d7edd568e63db6fcb2857329e294c655028
daeseopk <eotjqdlee@gmail.com>
2026-01-30 11:37:11 +0900

    feat: add global toast component

---
+++ src/common/providers/ToastProvider/toast.ts
@@ -0,0 +1,41 @@
+import type { ToastConfig } from "./context";
+
+type ToastHandler = {
+  show: (config: ToastConfig | string) => void;
+  hide: () => void;
+};
+
+let handler: ToastHandler | null = null;
+
+export const toast = {
+  show: (config: ToastConfig | string) => {
+    if (handler) {
+      handler.show(config);
+    }
+  },
+  hide: () => {
+    if (handler) {
+      handler.hide();
+    }
+  },
+  info: (message: string, options?: Omit<ToastConfig, "message" | "type">) => {
+    toast.show({ message, type: "info", ...options });
+  },
+  success: (message: string, options?: Omit<ToastConfig, "message" | "type">) => {
+    toast.show({ message, type: "success", ...options });
+  },
+  error: (message: string, options?: Omit<ToastConfig, "message" | "type">) => {
+    toast.show({ message, type: "error", ...options });
+  },
+  warning: (message: string, options?: Omit<ToastConfig, "message" | "type">) => {
+    toast.show({ message, type: "warning", ...options });
+  },
+};
+
+export function registerToastHandler(newHandler: ToastHandler) {
+  handler = newHandler;
+}
+
+export function unregisterToastHandler() {
+  handler = null;
+}

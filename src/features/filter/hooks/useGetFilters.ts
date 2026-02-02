8eaa21941b224d1c4a2f18d1b269140dbb27e35f
daeseopk <eotjqdlee@gmail.com>
2026-02-02 15:39:25 +0900

    refactor: move filter hooks into filter feature

---
+++ src/features/filter/hooks/useGetFilters.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { FILTER_QUERY_KEY } from "@/features/filter/constants/queryKey";
+import { filterService } from "@/features/filter/services/filterService";
+
+export function useGetFilters() {
+  return useQuery({
+    queryKey: [FILTER_QUERY_KEY.FILTERS],
+    queryFn: filterService.getFilters,
+  });
+}

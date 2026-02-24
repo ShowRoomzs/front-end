e65b3bb451de77363c4ed54be2bdcf8ca85aa1e2
daeseopk <eotjqdlee@gmail.com>
2026-02-24 16:37:11 +0900

    feat: invalidate inquiry history cache on create mutation

--- src/features/product/hooks/useCreateProductInquiryMutation.ts
+++ src/features/product/hooks/useCreateProductInquiryMutation.ts
@@ -1,5 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 
+import { queryClient } from "@/common/lib/queryClient";
+import { PRODUCT_INQUIRY_QUERY_KEY } from "@/features/product/constants/queryKey";
 import { productInquiryService } from "@/features/product/services/productInquiryService";
 import { ProductInquiryRequest } from "@/features/product/types/productInquiry";
 
@@ -7,5 +9,8 @@ export function useCreateProductInquiryMutation() {
   return useMutation({
     mutationFn: ({ productId, data }: { productId: number; data: ProductInquiryRequest }) =>
       productInquiryService.create(productId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [PRODUCT_INQUIRY_QUERY_KEY.INQUIRY_HISTORY] });
+    },
   });
 }

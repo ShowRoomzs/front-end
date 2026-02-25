b6c3ddef7cabad26928535c67fcf4e92f2836415
daeseopk <eotjqdlee@gmail.com>
2026-02-25 16:04:56 +0900

    feat: hide bottom tab in coupon navigator

--- src/navigators/CouponNavigator.tsx
+++ src/navigators/CouponNavigator.tsx
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect } from "react";
 
+import { useBottomTab } from "@/common/hooks/useBottomTab";
 import { COUPON_ROUTES } from "@/common/router/routes";
 import { CouponStackParamList } from "@/common/router/types";
 import CouponListView from "@/features/coupon/views/CouponListView";
@@ -8,6 +10,15 @@ import CouponRegisterView from "@/features/coupon/views/CouponRegisterView";
 const Stack = createNativeStackNavigator<CouponStackParamList>();
 
 export default function CouponNavigator() {
+  const { hide, show } = useBottomTab();
+
+  useEffect(() => {
+    hide();
+    return () => {
+      show();
+    };
+  }, [hide, show]);
+
   return (
     <Stack.Navigator initialRouteName={COUPON_ROUTES.LIST} screenOptions={{ headerShown: false }}>
       <Stack.Screen name={COUPON_ROUTES.LIST} component={CouponListView} />

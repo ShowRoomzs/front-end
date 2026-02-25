539226a9d4238e875a6c1954646168b8bd039fb1
daeseopk <eotjqdlee@gmail.com>
2026-02-25 14:29:38 +0900

    feat: add getDetail, update, and delete to productInquiryService

--- src/features/product/services/productInquiryService.ts
+++ src/features/product/services/productInquiryService.ts
@@ -2,6 +2,7 @@ import { apiInstance } from "@/common/lib/apiInstance";
 import { PageParams, PageResponse } from "@/common/types/page";
 import {
   ProductInquiryCategoryResponse,
+  ProductInquiryDetail,
   ProductInquiryHistory,
   ProductInquiryRequest,
 } from "@/features/product/types/productInquiry";
@@ -19,11 +20,28 @@ export const productInquiryService = {
 
     return response;
   },
+  update: async (inquiryId: number, data: ProductInquiryRequest) => {
+    const { data: response } = await apiInstance.put(`/user/product-inquiries/${inquiryId}`, data);
+
+    return response;
+  },
+  getDetail: async (inquiryId: number) => {
+    const { data: response } = await apiInstance.get<ProductInquiryDetail>(
+      `/user/product-inquiries/${inquiryId}`
+    );
+
+    return response;
+  },
   getHistory: async (params: PageParams) => {
     const { data: response } = await apiInstance.get<PageResponse<ProductInquiryHistory>>(
       "/user/product-inquiries",
       { params }
     );
+
+    return response;
+  },
+  delete: async (inquiryId: number) => {
+    const { data: response } = await apiInstance.delete(`/user/product-inquiries/${inquiryId}`);
 
     return response;
   },

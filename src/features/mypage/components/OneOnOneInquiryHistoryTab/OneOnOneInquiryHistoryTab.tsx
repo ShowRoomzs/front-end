00c69f9dd2b548f30575afda7e733d796cefd5b9
giirrr <ikl1121@naver.com>
2026-02-26 02:27:27 +0900

    feat: implement edit and delete actions in OneOnOneInquiryHistoryTab

--- src/features/mypage/components/OneOnOneInquiryHistoryTab/OneOnOneInquiryHistoryTab.tsx
+++ src/features/mypage/components/OneOnOneInquiryHistoryTab/OneOnOneInquiryHistoryTab.tsx
@@ -3,7 +3,10 @@ import { View } from "react-native";
 
 import PagingList from "@/common/components/PagingList/PagingList";
 import { useParams } from "@/common/hooks/useParams";
-import { useGetInquiries } from "@/features/inquiry/hooks/useGetInquiries";
+import { toast } from "@/common/providers/ToastProvider";
+import { useMypageNavigation } from "@/common/router";
+import { MYPAGE_ROUTES } from "@/common/router/routes";
+import { useGetInquiryHistory } from "@/features/inquiry/hooks/useGetInquiryHistory";
 import { useDeleteInquiryMutation } from "@/features/inquiry/hooks/useInquiryMutation/useDeleteInquiryMutation";
 import { InquiryHistory, InquiryHistoryParams } from "@/features/inquiry/types/inquiry";
 import OneOnOneInquiryHistoryItem from "@/features/mypage/components/OneOnOneInquiryHistoryItem/OneOnOneInquiryHistoryItem";
@@ -15,20 +18,31 @@ const INITIAL_PARAMS: InquiryHistoryParams = {
 
 export default function OneOnOneInquiryHistoryTab() {
   const { params } = useParams<InquiryHistoryParams>(INITIAL_PARAMS);
-  const { inquiries, pageInfo, isFetching, fetchNextPage } = useGetInquiries(params);
+  const { inquiries, pageInfo, isFetching, fetchNextPage } = useGetInquiryHistory(params);
   const { mutateAsync: deleteInquiry } = useDeleteInquiryMutation();
+  const navigation = useMypageNavigation();
 
   const handleLoadMore = useCallback(() => {
     fetchNextPage();
   }, [fetchNextPage]);
 
-  const handlePressEdit = useCallback((id: number) => {
-    console.log("id", id);
-  }, []);
+  const handlePressEdit = useCallback(
+    (id: number) => {
+      navigation.navigate(MYPAGE_ROUTES.INQUIRY_REGISTER, {
+        inquiryId: id,
+      });
+    },
+    [navigation]
+  );
 
   const handlePressDelete = useCallback(
-    (id: number) => {
-      return deleteInquiry(id);
+    async (id: number) => {
+      try {
+        await deleteInquiry(id);
+        toast.show("문의가 삭제되었습니다.");
+      } catch (error) {
+        console.error(error);
+      }
     },
     [deleteInquiry]
   );

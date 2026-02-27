dc583c9182301b6375f43b4f5f1b1050808b28e7
daeseopk <eotjqdlee@gmail.com>
2026-02-27 10:54:09 +0900

    feat: update app configuration for image permissions and add expo-document-picker dependency

--- app.config.ts
+++ app.config.ts
@@ -22,8 +22,8 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       supportsTablet: true,
       bundleIdentifier: "com.showroomz.app",
       infoPlist: {
-        NSPhotoLibraryUsageDescription:
-          "1:1 문의에 첨부할 사진을 선택하기 위해 갤러리 접근 권한이 필요합니다.",
+        NSPhotoLibraryUsageDescription: "이미지 업로드를 위해 사진 보관함 접근 권한이 필요합니다.",
+        NSCameraUsageDescription: "사진 촬영을 위해 카메라 접근이 필요합니다.",
         LSApplicationQueriesSchemes: [
           "naversearchapp",
           "naversearchthirdlogin",

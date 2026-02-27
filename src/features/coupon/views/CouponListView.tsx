213e53b8491c77a0ba0cd0a83e7a7dcf310de08a
daeseopk <eotjqdlee@gmail.com>
2026-02-27 16:43:48 +0900

    feat: integrate CouponCard component into CouponListView for improved coupon display

--- src/features/coupon/views/CouponListView.tsx
+++ src/features/coupon/views/CouponListView.tsx
@@ -4,6 +4,7 @@ import { View } from "react-native";
 import PagingList from "@/common/components/PagingList/PagingList";
 import { useCouponNavigation } from "@/common/router";
 import { COUPON_ROUTES } from "@/common/router/routes";
+import CouponCard from "@/features/coupon/components/CouponCard/CouponCard";
 import CouponListActions from "@/features/coupon/components/CouponListActions/CouponListActions";
 import CouponListHeader from "@/features/coupon/components/CouponListHeader/CouponListHeader";
 import { useGetCoupons } from "@/features/coupon/hooks/useGetCoupons";
@@ -25,7 +26,7 @@ export default function CouponListView() {
     navigation.navigate(COUPON_ROUTES.REGISTER);
   }, [navigation]);
 
-  const renderItem = useCallback(() => <View />, []);
+  const renderItem = useCallback(({ item }: { item: Coupon }) => <CouponCard coupon={item} />, []);
 
   return (
     <View className="flex-1 bg-gray0">
@@ -33,6 +34,8 @@ export default function CouponListView() {
       <PagingList<Coupon>
         data={coupons}
         pageInfo={pageInfo}
+        className="p-20"
+        contentContainerStyle={{ gap: 20 }}
         isLoading={isFetching}
         onLoadMore={handleLoadMore}
         renderItem={renderItem}

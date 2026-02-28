2acd1e91edcb40b5999cb2b54fe9c3b3645abdcf
giirrr <ikl1121@naver.com>
2026-03-01 05:21:03 +0900

    feat: add notice service, query keys, and data fetching hooks

---
+++ src/features/notice/hooks/useGetNoticeDetail.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { NOTICE_QUERY_KEY } from "../constants/queryKey";
+import { noticeService } from "../services/noticeService";
+
+export default function useGetNoticeDetail(noticeId: number) {
+  return useQuery({
+    queryKey: [NOTICE_QUERY_KEY.NOTICE_DETAIL, noticeId],
+    queryFn: () => noticeService.getNoticeDetail(noticeId),
+    enabled: !!noticeId,
+  });
+}

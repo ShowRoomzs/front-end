9f1681216b9672ad97bc2d6105c508ab31baf55c
giirrr <ikl1121@naver.com>
2026-03-02 16:07:34 +0900

    fix: make noticeDetail route params optional and update related hooks and view

--- src/features/notice/hooks/useGetNoticeDetail.ts
+++ src/features/notice/hooks/useGetNoticeDetail.ts
@@ -3,10 +3,10 @@ import { useQuery } from "@tanstack/react-query";
 import { NOTICE_QUERY_KEY } from "../constants/queryKey";
 import { noticeService } from "../services/noticeService";
 
-export default function useGetNoticeDetail(noticeId: number) {
+export default function useGetNoticeDetail(noticeId?: number) {
   return useQuery({
     queryKey: [NOTICE_QUERY_KEY.NOTICE_DETAIL, noticeId],
-    queryFn: () => noticeService.getNoticeDetail(noticeId),
+    queryFn: () => noticeService.getNoticeDetail(noticeId ?? 0),
     enabled: !!noticeId,
   });
 }

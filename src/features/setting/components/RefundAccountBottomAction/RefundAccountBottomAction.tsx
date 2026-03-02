468824fd7b59c4b8ddfff52806263cde0db72709
daeseopk <eotjqdlee@gmail.com>
2026-03-02 15:41:43 +0900

    feat: implement refund account registration UI

--- src/features/setting/components/RefundAccountBottomAction/RefundAccountBottomAction.tsx
+++ src/features/setting/components/RefundAccountBottomAction/RefundAccountBottomAction.tsx
@@ -4,11 +4,13 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Button from "@/common/components/Button/Button";
 
 interface RefundAccountBottomActionProps {
-  onPress?: () => void;
+  onPress: () => void;
+  disabled: boolean;
+  label: string;
 }
 
 export default function RefundAccountBottomAction(props: RefundAccountBottomActionProps) {
-  const { onPress } = props;
+  const { onPress, disabled, label } = props;
   const insets = useSafeAreaInsets();
 
   return (
@@ -16,8 +18,8 @@ export default function RefundAccountBottomAction(props: RefundAccountBottomActi
       className="absolute bottom-0 left-0 right-0 border-t border-gray2 bg-white px-10 pt-10"
       style={{ paddingBottom: insets.bottom }}
     >
-      <Button size="xl" variant="primary" onPress={onPress} disabled>
-        추가하기
+      <Button size="xl" variant="primary" onPress={onPress} disabled={disabled}>
+        {label}
       </Button>
     </View>
   );

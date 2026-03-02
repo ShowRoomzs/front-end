37df361ebf017f71ab14cc427082fb0b7cb05d12
daeseopk <eotjqdlee@gmail.com>
2026-03-02 14:31:26 +0900

    fix: update following count display format in MypageProfile for better readability

--- src/features/mypage/components/MypageProfile/MypageProfile.tsx
+++ src/features/mypage/components/MypageProfile/MypageProfile.tsx
@@ -64,8 +64,7 @@ export default function MypageProfile(props: MypageProfileProps) {
         <Pressable onPress={onPressFollowing}>
           <HStack gap={4} className="items-center">
             <Icon icon={COMMON_ASSETS.followingIcon} />
-            <Typography className="text-black font-medium text-13">{user.followingCount}</Typography>
-            <Typography className="text-black font-medium text-13">팔로잉</Typography>
+            <Typography className="text-black font-medium text-13">{`팔로잉${user.followingCount}`}</Typography>
             <Icon icon={COMMON_ASSETS.arrowRight} />
           </HStack>
         </Pressable>

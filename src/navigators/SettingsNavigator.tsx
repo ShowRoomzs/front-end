2af3dbcaa97ef39a4c482f92963e80a3b0ac94e8
daeseopk <eotjqdlee@gmail.com>
2026-03-02 16:28:39 +0900

    feat: add WithdrawalConfirmView

--- src/navigators/SettingsNavigator.tsx
+++ src/navigators/SettingsNavigator.tsx
@@ -7,6 +7,7 @@ import NicknameChangeView from "@/features/setting/views/NicknameChangeView";
 import NotificationSettingsView from "@/features/setting/views/NotificationSettingsView";
 import RefundAccountView from "@/features/setting/views/RefundAccountView";
 import SettingView from "@/features/setting/views/SettingView";
+import WithdrawalConfirmView from "@/features/setting/views/WithdrawalConfirmView";
 import WithdrawalView from "@/features/setting/views/WithdrawalView";
 
 const Stack = createNativeStackNavigator<SettingsStackParamList>();
@@ -20,6 +21,7 @@ export default function SettingsNavigator() {
       <Stack.Screen name={SETTINGS_ROUTES.NOTIFICATION_SETTINGS} component={NotificationSettingsView} />
       <Stack.Screen name={SETTINGS_ROUTES.REFUND_ACCOUNT} component={RefundAccountView} />
       <Stack.Screen name={SETTINGS_ROUTES.WITHDRAWAL} component={WithdrawalView} />
+      <Stack.Screen name={SETTINGS_ROUTES.WITHDRAWAL_CONFIRM} component={WithdrawalConfirmView} />
     </Stack.Navigator>
   );
 }

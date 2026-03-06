b391ef8d9a2f1d4419bea09001f9861ea7c89bd7
daeseopk <eotjqdlee@gmail.com>
2026-03-06 15:12:09 +0900

    refactor: add useInfiniteList hook and replace useInfiniteQuery in all hooks

---
+++ src/common/hooks/useInfiniteList.ts
@@ -0,0 +1,36 @@
+import { InfiniteData, QueryKey, useInfiniteQuery, UseInfiniteQueryOptions } from "@tanstack/react-query";
+
+import { PageInfo, PageResponse } from "@/common/types/page";
+
+type ContentOf<R> = R extends PageResponse<infer T> ? T : never;
+
+interface UseInfiniteListProps<R extends PageResponse<unknown>> extends Omit<
+  UseInfiniteQueryOptions<R, Error, InfiniteData<R, number>, QueryKey, number>,
+  "queryKey" | "queryFn" | "initialPageParam" | "getNextPageParam"
+> {
+  queryFn: (page: number) => Promise<R>;
+  queryKey: QueryKey;
+}
+
+export function useInfiniteList<R extends PageResponse<unknown>>(props: UseInfiniteListProps<R>) {
+  const { queryKey, queryFn, ...restQueryOptions } = props;
+
+  const query = useInfiniteQuery({
+    queryKey,
+    queryFn: ({ pageParam }) => queryFn(pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage: R) =>
+      lastPage.pageInfo.hasNext ? lastPage.pageInfo.currentPage + 1 : undefined,
+    ...restQueryOptions,
+  });
+
+  const content = (query.data?.pages.flatMap(page => page.content) ?? []) as Array<ContentOf<R>>;
+  const pageInfo: PageInfo | undefined = query.data?.pages.at(-1)?.pageInfo;
+
+  return {
+    ...query,
+    data: { content, pageInfo },
+    content,
+    pageInfo,
+  };
+}

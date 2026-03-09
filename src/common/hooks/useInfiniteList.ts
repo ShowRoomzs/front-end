fa2260286fbd9b729ba7117254fa622c7babc5f9
daeseopk <eotjqdlee@gmail.com>
2026-03-09 10:02:42 +0900

    refactor: update useInfiniteList to accept generic types directly, simplifying type handling in hooks

--- src/common/hooks/useInfiniteList.ts
+++ src/common/hooks/useInfiniteList.ts
@@ -2,27 +2,25 @@ import { InfiniteData, QueryKey, useInfiniteQuery, UseInfiniteQueryOptions } fro
 
 import { PageInfo, PageResponse } from "@/common/types/page";
 
-type ContentOf<R> = R extends PageResponse<infer T> ? T : never;
-
-interface UseInfiniteListProps<R extends PageResponse<unknown>> extends Omit<
-  UseInfiniteQueryOptions<R, Error, InfiniteData<R, number>, QueryKey, number>,
+interface UseInfiniteListProps<T> extends Omit<
+  UseInfiniteQueryOptions<PageResponse<T>, Error, InfiniteData<PageResponse<T>, number>, QueryKey, number>,
   "queryFn" | "initialPageParam" | "getNextPageParam"
 > {
-  queryFn: (page: number) => Promise<R>;
+  queryFn: (page: number) => Promise<PageResponse<T>>;
 }
 
-export function useInfiniteList<R extends PageResponse<unknown>>(props: UseInfiniteListProps<R>) {
+export function useInfiniteList<T>(props: UseInfiniteListProps<T>) {
   const { queryFn, ...restQueryOptions } = props;
 
   const query = useInfiniteQuery({
     queryFn: ({ pageParam }) => queryFn(pageParam),
     initialPageParam: 1,
-    getNextPageParam: (lastPage: R) =>
+    getNextPageParam: (lastPage: PageResponse<T>) =>
       lastPage.pageInfo.hasNext ? lastPage.pageInfo.currentPage + 1 : undefined,
     ...restQueryOptions,
   });
 
-  const content = (query.data?.pages.flatMap(page => page.content) ?? []) as Array<ContentOf<R>>;
+  const content: Array<T> = query.data?.pages.flatMap(page => page.content) ?? [];
   const pageInfo: PageInfo | undefined = query.data?.pages.at(-1)?.pageInfo;
 
   return {
--- src/features/wishlist/hooks/useGetWishlist.ts
+++ src/features/wishlist/hooks/useGetWishlist.ts
@@ -2,10 +2,10 @@ import { useInfiniteList } from "@/common/hooks/useInfiniteList";
 import { WISHLIST_QUERY_KEY } from "@/features/wishlist/constants/queryKey";
 import { wishlistService } from "@/features/wishlist/services/wishlistService";
 import { WishlistParams } from "@/features/wishlist/types/params";
-import { WishlistResponse } from "@/features/wishlist/types/wishlist";
+import { WishlistProductType } from "@/features/wishlist/types/wishlist";
 
 export function useGetWishlist(params: WishlistParams) {
-  return useInfiniteList<WishlistResponse>({
+  return useInfiniteList<WishlistProductType>({
     queryKey: [WISHLIST_QUERY_KEY.WISHLIST, params],
     queryFn: page => wishlistService.get({ ...params, page }),
   });
